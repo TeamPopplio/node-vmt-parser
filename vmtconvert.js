a400c1280f1bf6e2cba52e3b43c64f1839adeafc
Luis <office@luis-falkenhan.de>
2019-12-19 15:37:59 +0100

    Fixed Bug in Promise when using as npm-module

--- vmtconvert.js
+++ vmtconvert.js
@@ -13,40 +13,41 @@ class VMTConvert {
 	//TODO: Multidimensional?
 	static parseVMT(vmtFile){
 		return new Promise((resolve, reject) => {
-			fs.stat(vmtFile, function(err, stat) {
-				if(err == null) {
-					fs.readFile(vmtFile, 'utf8', function(err, contents) {
-						if(contents == null || contents == undefined) resolve(null);
-						let retArr={};
-						let lines = contents.replace(/\t/g, "").split("\r\n").filter(line => isRelevantLine(line));
-						for(let line of lines) {
-							let key = line.substring(line.lastIndexOf("$") + 1, line.indexOf(" "));
-							let value = line.replace(" ", "").replace("$", "").replace(key, "");
-							if(value.includes("\"[") && value.includes("]\"")){//Its a Array!
-								let tArr = value.replace("\"[", "").replace("]\"", "").split(" ").filter(e => !isNullOrEmpty(e));
-								for (let a = 0; a < tArr.length; a++) {
-									let val = tArr[a];
-									if(!isNaN(val)) tArr[a] = parseInt(val);//Its a Integer!
-									if(val.startsWith(".")) tArr[a] = parseFloat("0"+val);//Its a Float!
-								}
-								retArr[key] = tArr;
-							} else if(value.includes(".")){//Its a Decimal! (Float)
-								if(value.startsWith(".")) value = "0"+val;
-								retArr[key] = parseFloat(value);
-							} else if(!isNaN(value)){//Its a Integer!
-								retArr[key] = parseInt(value);
-							} else {//Its a String or something unsupported..
-								value = value.replace(/\"/g, "");
-								value = value.replace(/\\/g, "/");
-								retArr[key] = value;
-							}
+			try {
+				let contents = fs.readFileSync(vmtFile, 'utf8');
+				let retArr={};
+				if(contents == null || contents == undefined){ 
+					conslole.log("FILE EMPTY! - "+contents);
+					resolve(retArr);
+					return;
+				}
+				let lines = contents.replace(/\t/g, "").replace(/\r/g, "").split("\n").filter(line => isRelevantLine(line));
+				for(let line of lines) {
+					let key = line.substring(line.lastIndexOf("$") + 1, line.indexOf(" "));
+					let value = line.replace(" ", "").replace("$", "").replace(key, "").replace(/\r/g, "");
+					if(value.includes("\"[") && value.includes("]\"")){//Its a Array!
+						let tArr = value.replace("\"[", "").replace("]\"", "").split(" ").filter(e => !isNullOrEmpty(e));
+						for (let a = 0; a < tArr.length; a++) {
+							let val = tArr[a];
+							if(!isNaN(val)) tArr[a] = parseInt(val);//Its a Integer!
+							if(val.startsWith(".")) tArr[a] = parseFloat("0"+val);//Its a Float!
 						}
-						resolve(retArr);
-					});
-				} else if(err.code == 'ENOENT') {
-					resolve(null);
-				} 
-			});
+						retArr[key] = tArr;
+					} else if(value.includes(".")){//Its a Decimal! (Float)
+						if(value.startsWith(".")) value = "0"+val;
+						retArr[key] = parseFloat(value);
+					} else if(!isNaN(value)){//Its a Integer!
+						retArr[key] = parseInt(value);
+					} else {//Its a String or something unsupported..
+						value = value.replace(/\"/g, "");
+						value = value.replace(/\\/g, "/");
+						retArr[key] = value;
+					}
+				}
+				resolve(retArr);
+			} catch (err) {
+			  reject(err);
+			}
 		});
 	}
 }
